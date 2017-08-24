0235e51ccfaafee420bca3cab53d969234623b11
bluedevel <eurob@1896>
2017-08-24 07:37:32 +0200

    do stuff dynamic

--- testserver/src/main/java/test/bla/server/Server.java
+++ testserver/src/main/java/test/bla/server/Server.java
@@ -1,8 +1,11 @@
 package test.bla.server;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author Robin Engel on 24.08.17.
@@ -11,8 +14,11 @@ public class Server {
 
     public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
 
+        System.out.println(args[0]);
+        System.out.println(Arrays.toString(new File(args[0]).getParentFile().list()));
+
         URLClassLoader urlLoader = new URLClassLoader(
-                new URL[]{new URL("file:///home/rengel/IdeaProjects/test/testplugin/target/testplugin-1.0.0-SNAPSHOT.jar")},
+                new URL[]{new URL("file://" + args[0])},
                 Server.class.getClassLoader());
 
         Class<?> clazz = urlLoader.loadClass("bla.awesome.TestPlugin");
